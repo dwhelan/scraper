0c61b3cf09d146f1ae5b1c558feaf8eb58693028
Declan Whelan <declan@printchomp.com>
2013-06-12 00:12:13 -0400

    Refactored scraper

--- guerilla_price_scraper.rb
+++ guerilla_price_scraper.rb
@@ -7,57 +7,80 @@ class GuerillaPriceScraper
 
   def scrape(page_path)
     Capybara.default_driver = :selenium
+    Capybara.default_wait_time = 10
     visit "http://www.guerillaprinting.ca/#{page_path}"
     prices
   end
 
   def prices
-    result = {}
-    quantities.options.each do |quantity|
-      sizes.options.each do |size|
-        sides.options.each do |side|
-          finishes.options.each do |finish|
-            #result[[quantity, size, side, finish]] = price_for(quantity: quantity, size: size, side: side, finish: finish)
-            result[[quantity, size, side, finish]] = price_for(quantity, size, side, finish)
+    prices = {}
+    quantities.each do |quantity|
+      sizes.each do |size|
+        sides.each do |side|
+          finishes.each do |finish|
+            prices[[quantity, size, side, finish]] = price_for(quantity: quantity, size: size, side: side, finish: finish)
           end
         end
       end
     end
-    result
+    prices
   end
 
-  def price_for2(options)
+  private
+
+  def price_for(options)
     options.each do |key, option|
-      update_selection(lists[key], option)
+      select_list_option(lists[key], option)
     end
 
     price
   end
 
-  def price_for(quantity, size, side, finish)
-    update_selection(quantities, quantity)
-    update_selection(sizes, size)
-    update_selection(sides, side)
-    update_selection(finishes, finish)
-
-    price
-  end
-
-  def update_selection(list, option)
+  def select_list_option(list, option)
     old_price_container = price_container
-    wait_until { price_container != old_price_container } if list.select(option)
+    wait_until { price_updated(old_price_container) } if list.select(option)
   end
-
-
-  private
 
   def price_updated(old_price_container)
-    price_container_text = price_container
-    price_container_text != old_price_container and price_container_text != ''
+    new_price_container = price_container
+    new_price_container != old_price_container and new_price_container != ''
   end
 
   def price_container
     page.find('#updateDiv').text
+  end
+
+  def price
+    find('div#total div.calcPrice').text.gsub(/\D/, '').to_i
+  end
+
+  def lists
+    unless @lists
+      list_elements = all('.calcItem')
+      @lists ||= {
+        quantity: List.new(list_elements[0]),
+        size:     List.new(list_elements[1]),
+        side:     List.new(list_elements[2]),
+        finish:   List.new(list_elements[3]),
+    }
+    end
+    @lists
+  end
+
+  def quantities
+    lists[:quantity].options
+  end
+
+  def sizes
+    lists[:size].options
+  end
+
+  def sides
+    lists[:side].options
+  end
+
+  def finishes
+    lists[:finish].options
   end
 
   def wait_until
@@ -67,40 +90,4 @@ class GuerillaPriceScraper
     end
   end
 
-  def lists
-    @lists ||= {
-      quantity: List.new(calculatorSelections[0]),
-      size: List.new(calculatorSelections[1]),
-      side: List.new(calculatorSelections[2]),
-      finish: List.new(calculatorSelections[3]),
-    }
-  end
-
-  def quantities
-    @quantities ||= List.new(calculatorSelections[0])
-  end
-
-  def sizes
-    @sizes ||= List.new(calculatorSelections[1])
-  end
-
-  def sides
-    @sides ||= List.new(calculatorSelections[2])
-  end
-
-  def finishes
-    @finishes ||= List.new(calculatorSelections[3])
-  end
-
-  def price
-    find('div#total div.calcPrice').text.gsub(/\D/, '').to_i
-  end
-
-  def calculatorSelections
-    @calculatorSelections ||= all('.calcItem')
-  end
-
-  def options(select)
-    select.all('option').map { |node| node.text }
-  end
 end
